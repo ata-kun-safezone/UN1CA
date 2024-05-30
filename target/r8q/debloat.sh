c8c13df802f973f2fc2398474a49967be59dcf28
ata-kaner <ata.kaner03@gmail.com>
2024-05-30 09:43:13 +0300

    r8q: Cleanup debloat list

--- target/r8q/debloat.sh
+++ target/r8q/debloat.sh
@@ -20,64 +20,7 @@
 # - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
 # - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")
 
-# Overlays
-SYSTEM_DEBLOAT+="
-system/app/WifiRROverlayAppQC
-"
-PRODUCT_DEBLOAT+="
-overlay/SoftapOverlay6GHz
-overlay/SoftapOverlayOWE
-overlay/SoftapOverlayQC
-"
-
 # GameDriver
 SYSTEM_DEBLOAT+="
-system/priv-app/GameDriver-SM8550
-"
-
-# Mocca
-SYSTEM_DEBLOAT+="
-system/app/MoccaMobile
-"
-
-# eSE
-SYSTEM_DEBLOAT+="
-system/bin/esecos_daemon
-system/etc/init/esecos.rc
-"
-
-# system_ext clean-up
-SYSTEM_EXT_DEBLOAT+="
-app/QCC
-bin/qccsyshal@1.2-service
-etc/permissions/com.qti.location.sdk.xml
-etc/permissions/com.qualcomm.location.xml
-etc/permissions/privapp-permissions-com.qualcomm.location.xml
-framework/com.qti.location.sdk.jar
-framework/oat/arm/com.qti.location.sdk.art
-framework/oat/arm/com.qti.location.sdk.odex
-framework/oat/arm/com.qti.location.sdk.vdex
-framework/oat/arm64/com.qti.location.sdk.art
-framework/oat/arm64/com.qti.location.sdk.odex
-framework/oat/arm64/com.qti.location.sdk.vdex
-lib/libqcc.so
-lib/libqcc_file_agent_sys.so
-lib/libqccdme.so
-lib/libqccfileservice.so
-lib/vendor.qti.hardware.qccsyshal@1.0.so
-lib/vendor.qti.hardware.qccsyshal@1.1.so
-lib/vendor.qti.hardware.qccsyshal@1.2.so
-lib/vendor.qti.hardware.qccvndhal@1.0.so
-lib/vendor.qti.qccvndhal_aidl-V1-ndk.so
-lib64/libqcc.so
-lib64/libqcc_file_agent_sys.so
-lib64/libqccdme.so
-lib64/libqccfileservice.so
-lib64/vendor.qti.hardware.qccsyshal@1.0.so
-lib64/vendor.qti.hardware.qccsyshal@1.1.so
-lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so
-lib64/vendor.qti.hardware.qccsyshal@1.2.so
-lib64/vendor.qti.hardware.qccvndhal@1.0.so
-lib64/vendor.qti.qccvndhal_aidl-V1-ndk.so
-priv-app/com.qualcomm.location
+system/priv-app/GameDriver-SM8350
 "
